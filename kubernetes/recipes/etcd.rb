7e034003ed7fda12e7b6d48ebfb7b1f2b2abc62c
ab01 <ankitbhalla786@gmail.com>
2018-02-14 12:33:53 +0530

    update etcd.rb
    update etcd.rb

--- kubernetes/recipes/etcd.rb
+++ kubernetes/recipes/etcd.rb
@@ -4,9 +4,9 @@ bash 'install_etcd' do
   code <<-EOH
   if [ ! -f /usr/local/bin/etcd ]; then
     yum install wget -y
-    wget --max-redirect 255 https://github.com/coreos/etcd/releases/download/v2.2.5/etcd-v2.2.5-linux-amd64.tar.gz
-    tar zxvf etcd-v2.2.5-linux-amd64.tar.gz
-    cd etcd-v2.2.5-linux-amd64
+    wget --max-redirect 255 https://github.com/coreos/etcd/releases/download/v2.3.6/etcd-v2.3.6-linux-amd64.tar.gz
+    tar zxvf etcd-v2.3.6-linux-amd64.tar.gz
+    cd etcd-v2.3.6-linux-amd64
     cp etcd etcdctl /usr/local/bin
   fi
   EOH
